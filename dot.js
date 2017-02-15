0cd392d27295ce4166634a9da1b5cf2e2c81a273
Geoffrey <geo.galagaran@gmail.com>
2017-02-15 13:15:31 +0800

    [CanvasOBJ] 	remove: 		draw, setDim, and getDim properties on the canvas return object 	add: 		context property to the canvas return object
    [DotPROTO]
    	implement:
    		properties:
    		[posX, posY]
    			- position of the "dot" in the canvas
    		[speedX, speedY]
    			- "speed" of the dots on the canvas; increment/decrements
    			depending on whether it goes outside the canvas viewport
    		[size]
    			- size of the dot
    
    [CreateDotsFUNC]
    	implement:
    		functionality; takes and integer n, posX, posY where 'n' is the size
    		of how many Dot instances to make with posX, posY as its starting
    		coordinates; returns an array of Dots
    [InitFUNC]
    	implement:
    		functionality; iterate and draw each dot instances created from the [CreateDotsFUNC].
    		Use their posX, posY, and size properties on the arc method from the context, animate
    		or redraw and change their positions through their speed properties; On window resize;
    		resize the canvas and create another redeclare a different dot array

--- dot.js
+++ dot.js
@@ -5,8 +5,6 @@ const canvas = (function() {
   const width  = context.canvas.width  = window.innerWidth
   const height = context.canvas.height = window.innerHeight
 
-  window.onresize = canvasResize
-
   function draw(callback) {
     callback(context)
   }
@@ -18,28 +16,90 @@ const canvas = (function() {
     return { width, height }
   }
 
-  function canvasResize() {
-    context.canvas.width = window.innerWidth
-    context.canvas.height = window.innerHeight
-  }
-
-
-  return { draw, dim }
+  return { context, dim }
 })()
 
-class Dots {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
+class Dot {
+  constructor(posX, posY, speedX, speedY) {
+    this.posX   = posX
+    this.posY   = posY
+    this.speedX = speedX
+    this.speedY = speedY
+    this.size   = 4
   }
 }
 
+function createDots(n, width, height) {
+  const dots = []
+
+  function randSign() {
+    return Math.round(Math.random()) ? 1 : -1
+  }
+
+  for(let i = 0; i < n; i++) {
+    let randX  = Math.floor(Math.random() * width)
+    let randY  = Math.floor(Math.random() * height)
+    let speedX = 1 * randSign()
+    let speedY = 1 * randSign()
+
+    dots.push(new Dot(randX, randY, speedX, speedY))
+  }
+
+  return dots
+}
+
+
+
 ;(function() {
-  canvas.draw(ctx => {
-    let width  = canvas.dim().width
-    let height = canvas.dim().height
+  const ctx        = canvas.context
+  const item_size  = 50
 
-    
-  })
+  let width  = canvas.dim().width
+  let height = canvas.dim().height
+  let dots   = createDots(item_size, width, height)
 
+  window.onresize = redraw
+
+  function redraw() {
+    width  = ctx.canvas.width  = window.innerWidth
+    height = ctx.canvas.height = window.innerHeight
+    dots   = createDots(item_size, width, height)
+  }
+
+
+  function draw() {
+    ctx.clearRect(0, 0, width, height)
+
+    dots.forEach( dot => {
+      console.log(dot.posX, dot.posY)
+
+      dot.posX = dot.posX + dot.speedX
+      dot.posY = dot.posY + dot.speedY
+
+      if(dot.posX > width) {
+        dot.speedX = -dot.speedX
+      }
+
+      if(dot.posY > height) {
+        dot.speedY = -dot.speedY
+      }
+
+      if(dot.posX < 0) {
+        dot.speedX = Math.abs(dot.speedX)
+      }
+
+      if(dot.posY < 0) {
+        dot.speedY = Math.abs(dot.speedY)
+      }
+
+      ctx.fillStyle = "#2A5";
+      ctx.beginPath();
+      ctx.arc(dot.posX, dot.posY, dot.size, 0, Math.PI * 2);
+      ctx.fill();
+    })
+
+    requestAnimationFrame(draw)
+  }
+
+  draw()
 })()
